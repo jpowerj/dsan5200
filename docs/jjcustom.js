ebb92922d9d4ed302888f10c49e9a1252b632053
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-02-28 17:13:33 -0500

    zoom link

--- docs/jjcustom.js
+++ docs/jjcustom.js
@@ -1,4 +1,4 @@
 window.document.addEventListener("DOMContentLoaded", function (event) {
-    var $subtitleDiv = $("<br><span class='subtitle'>Tues, 12:30-3pm, Car Barn 309</span><br><span class='subtitle'>Prof. Jeff Jacobs&nbsp;<a href='mailto:jj1088@georgetown.edu' target='_blank'><i class='bi bi-envelope-at ps-1 pe-1'></i></a></span><br><span class='subtitle'>Spring 2024, Georgetown</span>");
+    var $subtitleDiv = $("<br><span class='subtitle'>Tues, 12:30-3pm, Car Barn 309</span><br><span class='subtitle'>Prof. Jeff Jacobs&nbsp;<a href='mailto:jj1088@georgetown.edu' target='_blank'><i class='bi bi-envelope-at ps-1 pe-1'></i></a></span><br><span class='subtitle'>Spring 2024, Georgetown</span><br><a class='subtitle' href='https://georgetown.zoom.us/j/93322544924' target='_blank'>Recurring Zoom Link <i class='bi bi-box-arrow-up-right ps-1'></i></a>");
     $('.sidebar-title').append($subtitleDiv);
   });
